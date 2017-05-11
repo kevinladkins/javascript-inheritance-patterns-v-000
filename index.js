f2ee5c938bae246b63391486748d2431206e04d3
Kevin Adkins <kevinladkins@gmail.com>
2017-05-11 17:50:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function Shape(sides, x, y) {
+  this.sides = sides;
+  this.x = x;
+  this.y = y;
+}
+
+function Rectangle(x, y, width, height) {
+  Shape.call(this, "4", x, y) 
+  this.width = width;
+  this.height = height;
+}
